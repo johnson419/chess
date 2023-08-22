12b06a3ef62d67d72ec23103c02e60789cb8091d
Johnson Fredrick <johnsonfredrick577@gmail.com>
2023-08-22 12:41:05 +0300

    added necessary scripts and styling

---
+++ scripts/main.js
@@ -0,0 +1,15 @@
+// scripts/main.js
+const Chess = require('chess.js'); // Import the chess.js library
+
+const board = document.getElementById('chessboard');
+const game = new Chess(); // Initialize the chess game
+
+// TODO: Implement your chessboard rendering and interaction code here
+
+// Example: Render the initial chessboard
+function renderChessboard() {
+    // TODO: Implement rendering logic here
+}
+
+// Call the function to render the initial chessboard
+renderChessboard();
